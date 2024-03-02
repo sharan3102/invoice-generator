02f8773a1676bb2fc67d0820fecb18f741f88dec
sharan3102 <2020it0134@svce.ac.in>
2024-03-02 18:43:21 +0530

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { text, image, barcodes } from "@pdfme/schemas";
+import { generate } from "@pdfme/generator";
 import InvoiceForm from './components/Form/InvoiceForm'
+
 function App() {
-  const [count, setCount] = useState(0)
+  const generatePDF = async (formData) => {
+    // Load base PDF dynamically
+    const basePdfResponse = await fetch('src/assets/EmptyInvoiceFinal.pdf');
+    const basePdfData = await basePdfResponse.arrayBuffer();
+    const CHUNK_SIZE = 0xFFFF; // Chunk size to avoid Maximum call stack size exceeded error
+
+    function arrayBufferToBase64(buffer) {
+      let binary = '';
+      const bytes = new Uint8Array(buffer);
+
+      for (let i = 0; i < bytes.byteLength; i += CHUNK_SIZE) {
+        const chunk = bytes.subarray(i, i + CHUNK_SIZE);
+        binary += String.fromCharCode.apply(null, chunk);
+      }
+
+      return btoa(binary);
+    }
+
+const basePdfBase64 = arrayBufferToBase64(basePdfData);
+
+
+    // Template definition
+    const template = {
+      schemas: [
+        {
+          date: {
+            "type": "text",
+            "position": {
+              "x": 119,
+              "y": 216
+            },
+            "width": 40,
+            "height": 16,
+            "fontSize": 10,
+            "fontColor": "#000E8A"
+          }
+        },
+      ],
+      basePdf: `data:application/pdf;base64,${basePdfBase64}`,
+    };
+
+    // Plugins
+    const plugins = { text, image, qrcode: barcodes.qrcode };
+
+    // Inputs
+    const inputs = [formData];
+
+    // Generate PDF
+    const pdf = await generate({ template, plugins, inputs });
+
+    // Download the PDF
+    const blob = new Blob([pdf.buffer], { type: 'application/pdf' });
+    const fileName = 'invoice.pdf';
+
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   return (
-    <>
-      <InvoiceForm/>
-    </>
-    
-  )
+    <div>
+      <InvoiceForm generatePDF={generatePDF} />
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Form/InvoiceForm.jsx
+++ src/components/Form/InvoiceForm.jsx
@@ -1,8 +1,47 @@
-// InvoiceForm.js
-import React from "react";
+import React, { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const InvoiceForm = ({ generatePDF }) => {
+  const [formData, setFormData] = useState({
+    companyName: '',
+    billNo: '0',
+    date: '0',
+    vehicleNo: '0',
+    typeOfVehicle: '0',
+    driverName: '0',
+    closingKm: '0',
+    startingKm: '0',
+    totalKm: '0',
+    closingTime: '0',
+    startingTime: '0',
+    totalHoursUsed: '0',
+    particularsOfDuty: '0',
+    arrangedBy: '0',
+    reportedTo: '0',
+    hireCharges: '0',
+    fuelChargesPerKm: '0',
+    excessPerKm: '0',
+    parkingCharges: '0',
+    driversBatta: '0',
+    permitCharges: '0',
+    nightHaltCharges: '0',
+    totalCharges: '0',
+    lessAdvance: '0',
+    balance: '0',
+    moneyInRupees: '0',
+  });
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleGeneratePDF = () => {
+    generatePDF(formData);
+  };
+
   return (
     <div className="container mt-5">
       <h4 className="mb-5">Invoice Generator 💌</h4>
@@ -10,77 +49,77 @@ const InvoiceForm = ({ generatePDF }) => {
         {/* Company Name */}
         <div className="form-group">
           <label htmlFor="companyName">Company Name:</label>
-          <select className="form-control" id="companyName" name="companyName" required defaultValue="">
-            <option defaultValue="">Select Company Name</option>
-            <option defaultValue="Kumar Travels">Kumar Travels</option>
-            <option defaultValue="Viveka Travels">Viveka Travels</option>
+          <select className="form-control" id="companyName" name="companyName" required value={formData.companyName} onChange={handleInputChange}>
+            <option value="">Select Company Name</option>
+            <option value="Kumar Travels">Kumar Travels</option>
+            <option value="Viveka Travels">Viveka Travels</option>
           </select>
         </div>
 
         {/* Bill No. */}
         <div className="form-group">
           <label htmlFor="billNo">Bill No:</label>
-          <input type="text" className="form-control" id="billNo" name="billNo" defaultValue="0" required />
+          <input type="text" className="form-control" id="billNo" name="billNo" value={formData.billNo} onChange={handleInputChange} required />
         </div>
 
         {/* Date */}
         <div className="form-group">
           <label htmlFor="date">Date:</label>
-          <input type="date" className="form-control" id="date" name="date" defaultValue="0" required />
+          <input type="date" className="form-control" id="date" name="date" value={formData.date} onChange={handleInputChange} required />
         </div>
 
         {/* Vehicle No */}
         <div className="form-group">
           <label htmlFor="vehicleNo">Vehicle No:</label>
-          <input type="text" className="form-control" id="vehicleNo" name="vehicleNo" defaultValue="0" required />
+          <input type="text" className="form-control" id="vehicleNo" name="vehicleNo" value={formData.vehicleNo} onChange={handleInputChange} required />
         </div>
 
         {/* Type of Vehicle */}
         <div className="form-group">
           <label htmlFor="typeOfVehicle">Type of Vehicle:</label>
-          <input type="text" className="form-control" id="typeOfVehicle" name="typeOfVehicle" defaultValue="0" required />
+          <input type="text" className="form-control" id="typeOfVehicle" name="typeOfVehicle" value={formData.typeOfVehicle} onChange={handleInputChange} required />
         </div>
 
         {/* Driver Name */}
         <div className="form-group">
           <label htmlFor="driverName">Driver Name:</label>
-          <input type="text" className="form-control" id="driverName" name="driverName" defaultValue="0" required />
+          <input type="text" className="form-control" id="driverName" name="driverName" value={formData.driverName} onChange={handleInputChange} required />
         </div>
 
         {/* Closing KM */}
         <div className="form-group">
           <label htmlFor="closingKm">Closing Km:</label>
-          <input type="text" className="form-control" id="closingKm" name="closingKm" defaultValue="0" required />
+          <input type="text" className="form-control" id="closingKm" name="closingKm" value={formData.closingKm} onChange={handleInputChange} required />
         </div>
 
         {/* Starting KM */}
         <div className="form-group">
           <label htmlFor="startingKm">Starting Km:</label>
-          <input type="text" className="form-control" id="startingKm" defaultValue="0" name="startingKm" required />
+          <input type="text" className="form-control" id="startingKm" name="startingKm" value={formData.startingKm} onChange={handleInputChange} required />
         </div>
 
         {/* Total KM */}
         <div className="form-group">
           <label htmlFor="totalKm">Total Km:</label>
-          <input type="text" className="form-control" id="totalKm" defaultValue="0" name="totalKm" required />
+          <input type="text" className="form-control" id="totalKm" name="totalKm" value={formData.totalKm} onChange={handleInputChange} required />
         </div>
 
         {/* Closing Time */}
         <div className="form-group">
           <label htmlFor="closingTime">Closing Time:</label>
-          <input type="text" className="form-control" id="closingTime" name="closingTime" defaultValue="0" required />
+          <input type="text" className="form-control" id="closingTime" name="closingTime" value={formData.closingTime} onChange={handleInputChange} required />
         </div>
 
         {/* Starting Time */}
         <div className="form-group">
           <label htmlFor="startingTime">Starting Time:</label>
-          <input type="text" className="form-control" id="startingTime" name="startingTime" defaultValue="0" required />
+          <input type="text" className="form-control" id="startingTime" name="startingTime" value={formData.startingTime} onChange={handleInputChange} required />
         </div>
 
         {/* Total Hours Used */}
         <div className="form-group">
           <label htmlFor="totalHoursUsed">Total Hours Used:</label>
-          <input type="text" className="form-control" id="totalHoursUsed" name="totalHoursUsed" defaultValue="0" required />
+          <input type="text" className="form-control" id="totalHoursUsed" name="totalHoursUsed" value={formData.totalHoursUsed} onChange={handleInputChange} required />
         </div>
 
         {/* Particulars */}
@@ -91,7 +130,8 @@ const InvoiceForm = ({ generatePDF }) => {
             className="form-control"
             id="particularsOfDuty"
             name="particularsOfDuty"
-            defaultValue="0"
+            value={formData.particularsOfDuty}
+            onChange={handleInputChange}
             required
           />
         </div>
@@ -99,19 +139,19 @@ const InvoiceForm = ({ generatePDF }) => {
         {/* Arranged By */}
         <div className="form-group">
           <label htmlFor="arrangedBy">Arranged By:</label>
-          <input type="text" className="form-control" id="arrangedBy" name="arrangedBy" defaultValue="0" required />
+          <input type="text" className="form-control" id="arrangedBy" name="arrangedBy" value={formData.arrangedBy} onChange={handleInputChange} required />
         </div>
 
         {/* Reported To */}
         <div className="form-group">
           <label htmlFor="reportedTo">Reported To:</label>
-          <input type="text" className="form-control" id="reportedTo" defaultValue="0" name="reportedTo" required />
+          <input type="text" className="form-control" id="reportedTo" name="reportedTo" value={formData.reportedTo} onChange={handleInputChange} required />
         </div>
 
         {/* Hire Charges */}
         <div className="form-group">
           <label htmlFor="hireCharges">Hire Charges:</label>
-          <input type="text" className="form-control" id="hireCharges" defaultValue="0" name="hireCharges" required />
+          <input type="text" className="form-control" id="hireCharges" name="hireCharges" value={formData.hireCharges} onChange={handleInputChange} required />
         </div>
 
         {/* Fuel Charges Per Km */}
@@ -121,8 +161,9 @@ const InvoiceForm = ({ generatePDF }) => {
             type="text"
             className="form-control"
             id="fuelChargesPerKm"
-            defaultValue="0"
             name="fuelChargesPerKm"
+            value={formData.fuelChargesPerKm}
+            onChange={handleInputChange}
             required
           />
         </div>
@@ -130,7 +171,7 @@ const InvoiceForm = ({ generatePDF }) => {
         {/* Excess Per Km */}
         <div className="form-group">
           <label htmlFor="excessPerKm">Excess Per Km:</label>
-          <input type="text" className="form-control" id="excessPerKm" defaultValue="0" name="excessPerKm" required />
+          <input type="text" className="form-control" id="excessPerKm" name="excessPerKm" value={formData.excessPerKm} onChange={handleInputChange} required />
         </div>
 
         {/* Parking Charges */}
@@ -140,8 +181,9 @@ const InvoiceForm = ({ generatePDF }) => {
             type="text"
             className="form-control"
             id="parkingCharges"
-            defaultValue="0"
             name="parkingCharges"
+            value={formData.parkingCharges}
+            onChange={handleInputChange}
             required
           />
         </div>
@@ -149,13 +191,13 @@ const InvoiceForm = ({ generatePDF }) => {
         {/* Driver's Batta */}
         <div className="form-group">
           <label htmlFor="driversBatta">Driver's Batta:</label>
-          <input type="text" className="form-control" defaultValue="0" id="driversBatta" name="driversBatta" required />
+          <input type="text" className="form-control" id="driversBatta" name="driversBatta" value={formData.driversBatta} onChange={handleInputChange} required />
         </div>
 
         {/* Permit Charges */}
         <div className="form-group">
           <label htmlFor="permitCharges">Permit Charges:</label>
-          <input type="text" defaultValue="0" className="form-control" id="permitCharges" name="permitCharges" required />
+          <input type="text" className="form-control" id="permitCharges" name="permitCharges" value={formData.permitCharges} onChange={handleInputChange} required />
         </div>
 
         {/* Night Halt Charges */}
@@ -165,8 +207,9 @@ const InvoiceForm = ({ generatePDF }) => {
             type="text"
             className="form-control"
             id="nightHaltCharges"
-            defaultValue="0"
             name="nightHaltCharges"
+            value={formData.nightHaltCharges}
+            onChange={handleInputChange}
             required
           />
         </div>
@@ -174,19 +217,19 @@ const InvoiceForm = ({ generatePDF }) => {
         {/* Total Charges */}
         <div className="form-group">
           <label htmlFor="totalCharges">Total Charges:</label>
-          <input type="text" className="form-control" id="totalCharges" name="totalCharges" defaultValue="0" required />
+          <input type="text" className="form-control" id="totalCharges" name="totalCharges" value={formData.totalCharges} onChange={handleInputChange} required />
         </div>
 
         {/* Less Advance */}
         <div className="form-group">
           <label htmlFor="lessAdvance">Less Advance:</label>
-          <input type="text" className="form-control" id="lessAdvance" name="lessAdvance" defaultValue="0" required />
+          <input type="text" className="form-control" id="lessAdvance" name="lessAdvance" value={formData.lessAdvance} onChange={handleInputChange} required />
         </div>
 
         {/* Balance */}
         <div className="form-group">
           <label htmlFor="balance">Balance:</label>
-          <input type="text" className="form-control" id="balance" name="balance" defaultValue="0" required />
+          <input type="text" className="form-control" id="balance" name="balance" value={formData.balance} onChange={handleInputChange} required />
         </div>
 
         {/* Money in Rupees */}
@@ -197,7 +240,8 @@ const InvoiceForm = ({ generatePDF }) => {
             className="form-control"
             id="moneyInRupees"
             name="moneyInRupees"
-            defaultValue="0"
+            value={formData.moneyInRupees}
+            onChange={handleInputChange}
             required
           />
         </div>
@@ -227,7 +271,7 @@ const InvoiceForm = ({ generatePDF }) => {
         </div>
 
         {/* Generate Button */}
-        <button type="button" className="btn btn-primary mb-5" onClick={generatePDF}>
+        <button type="button" className="btn btn-primary mb-5" onClick={handleGeneratePDF}>
           Generate Invoice
         </button>
       </form>
